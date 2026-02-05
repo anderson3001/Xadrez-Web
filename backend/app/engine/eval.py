e96e050ca886e31271779ec607375c5c89649b98
Anderson Gomes <derson3001@gmail.com>
2026-02-04 23:44:07 -0300

    feat: valores por peça e por posição

---
+++ backend/app/engine/eval.py
@@ -0,0 +1,122 @@
+import chess
+
+PIECE_VALUES = {
+    chess.PAWN: 100,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.ROOK: 500,
+    chess.QUEEN: 900,
+    chess.KING: 20000
+}
+
+PAWN_TABLE = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+]
+
+KNIGHT_TABLE = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50,
+]
+
+BISHOP_TABLE = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20,
+]
+
+ROOK_TABLE = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0
+]
+
+QUEEN_TABLE = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+      0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+]
+
+KING_TABLE = [
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+     20, 20,  0,  0,  0,  0, 20, 20,
+     20, 30, 10,  0,  0, 10, 30, 20
+]
+
+def evaluate_board(board: chess.Board):
+    if board.is_checkmate():
+        if board.turn == chess.WHITE:
+            return -99999
+        else:
+            return 99999
+    
+    if board.is_game_over():
+        return 0
+
+    score = 0
+
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        
+        if piece:
+            value = PIECE_VALUES.get(piece.piece_type, 0)
+
+            pst_bonus = 0
+            table = None
+            if piece.piece_type == chess.PAWN:
+                table = PAWN_TABLE
+            elif piece.piece_type == chess.KNIGHT:
+                table = KNIGHT_TABLE
+            elif piece.piece_type == chess.BISHOP:
+                table = BISHOP_TABLE
+            elif piece.piece_type == chess.ROOK:
+                table = ROOK_TABLE
+            elif piece.piece_type == chess.QUEEN:
+                table = QUEEN_TABLE
+            elif piece.piece_type == chess.KING:
+                table = KING_TABLE
+
+            pst_bonus = 0
+            if table:
+                if piece.color == chess.WHITE:
+                    pst_bonus = table[square]
+                else:
+                    pst_bonus = -table[chess.square_mirror(square)]
+            if piece.color == chess.WHITE:
+                score += (value + pst_bonus)
+            else:
+                score -= (value + pst_bonus)
+                
+    return score
