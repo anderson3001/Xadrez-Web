9806118247c2bce085409b3865a9312783f87fbc
Anderson Gomes <derson3001@gmail.com>
2026-02-05 20:25:28 -0300

    feat: livro de aberturas para o bot

--- backend/app/engine/bot.py
+++ backend/app/engine/bot.py
@@ -1,9 +1,18 @@
 import chess 
+import chess.polyglot
 
 from app.engine.search import find_best_move
 
 def calculate_best_move(fen: str):
     board = chess.Board(fen)
+
+    try:
+        with chess.polyglot.open_reader("app/engine/gm2001.bin") as reader:
+            entry = reader.weighted_choice(board)     
+            return entry.move.uci()
+            
+    except:
+        pass
 
     if board.is_game_over():
         return None
